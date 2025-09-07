c0a63a778331207364c761d2e7f2c0c096262f67
Lyudmila <lyudmilabravo@gmail.com>
2025-09-07 15:00:22 +0700

    начало

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,30 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest;
+    xhr.responseType = 'json';
 
-};
+    let url = options.url;
+    const formData = new FormData();
+
+    if (options.method === 'GET') {
+        for (let key in options.data) {
+            url = options.data;
+        }
+    } else {
+        for (let key in options.data) {
+            formData =
+        }
+    }
+
+    try {
+        xhr.open(options.method, url);
+        xhr.send(formData);
+    }
+    catch (e) {
+        // перехват сетевой ошибки
+        callback(e);
+    }
+}
+
+xhr.onload = function () {
+    options.callback(null, xhr.response);
+}
